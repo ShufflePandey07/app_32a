59a2e0b4855820167b217b3726440b087fe2baad
ShufflePandey07 <220056@softwarica.edu.np>
2024-04-26 09:45:47 +0545

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
+import Homepage from "./pages/homepage/Homepage";
+import Loginpage from "./pages/login/Loginpage";
+import Registerpage from "./pages/register/Registerpage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/register' element={<Registerpage />} />
+        <Route path='/login' element={<Loginpage />} />
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/register/Registerpage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Registerpage = () => {
+    return (
+        <div>Register page</div>
+    )
+}
+export default Registerpage
